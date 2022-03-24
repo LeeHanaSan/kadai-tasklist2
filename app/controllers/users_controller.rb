4995fc77ef25f8caeac680cfa01de507703d55d5
Arisa Ichigaya <prog.ichigaya@gmail.com>
2022-03-24 12:20:54 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,5 @@
 class UsersController < ApplicationController
-  def index
-    @user = User.order(id: :desc)
-  end
-
-  def show
-    @user = User.find(params[:id])
-  end
-
+  
   def new
     @user = User.new
   end
